80e6afb3e721004245fc0f4918d4091e97ba65e0
SkyAlice <270750731+SkyAlice-source@users.noreply.github.com>
2026-08-20 01:31:39 +0800

    chore: flutter_markdown → flutter_markdown_plus（discontinued 迁移）

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_reorderable_grid_view/widgets/reorderable_builder.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:url_launcher/url_launcher_string.dart';
